f2011d9e32327889d250b802306dd09c407da01d
cesardddd <92449662+CesarFloresH@users.noreply.github.com>
2022-05-11 11:12:59 -0600

    Se creo el nucleo del sevidor en app.js y se modifico la version del package.json.

--- ProyectoExpress/app.js
+++ ProyectoExpress/app.js
@@ -1,4 +1,23 @@
-var express = require('express');
+const express = require('express');
+const port = 3002;
+
+const bodyParser = require('body-parser');
+const routes = require('./routes/routes');
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true,
+}))
+
+routes(app);
+
+const server = app.listen(port, (error) => {
+    if(error) return console.log('Error: ${error}');
+    console.log('El servidor escucha en el puerto ${server.address().port}');
+});
+
+/*var express = require('express');
 var app = express();
 
 app.get('/hola', function(req,res){
@@ -10,4 +29,4 @@ app.get('/adios', function(req,res){
 });
 app.listen(3000, function(){
     console.log('Aplicacion ejemplo, escuchando el puerto 3000!')
-});
+});*/

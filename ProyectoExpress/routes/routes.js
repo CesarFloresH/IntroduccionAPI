4142968b2bb5097a95c5489162727633c22fc400
cesardddd <92449662+CesarFloresH@users.noreply.github.com>
2022-05-12 11:29:52 -0600

    Se agrego una nueva ruta para actualizar un usuario existente y se modifico la version del pckage.json.

--- ProyectoExpress/routes/routes.js
+++ ProyectoExpress/routes/routes.js
@@ -23,10 +23,19 @@ const router = app =>{
             response.send(result);
         });
     });
+    //Agregar un nuevo usuario
     app.post('/users', (request, response) =>{
         pool.query('INSERT INTO users SET ?', request.body , (error, result) =>{
             if(error) throw error;
             response.status(201).send(`User added with ID: ${result.insertId}`);
+        });
+    });
+    //Actualizar un usuario
+    app.put('/users/:id', (request, response) =>{
+        const id = request.params.id;
+        pool.query('UPDATE users SET ? WHERE id = ?', [request.body, id] , (error, result) =>{
+            if(error) throw error;
+            response.send('User update successfully.');
         });
     });
 }
